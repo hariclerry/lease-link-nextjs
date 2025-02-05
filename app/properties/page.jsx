1c7405f1482213004d32059dcbdf1e966497b1b0
hariclerry <clerryha@gmail.com>
2025-02-05 15:33:03 -0600

    add pagination implementation

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import PropertyCard from '@/components/PropertyCard';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import Pagination from '@/components/Pagination';
 
-const PropertiesPage =  async() => {
+const PropertiesPage =  async({searchParams: {page=1, pageSize=9}}) => {
    await connectDB();
-   const properties = await Property.find({}).lean();
+   const skip = (page - 1) * pageSize;
+   const total = await Property.countDocuments({});
+   const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+   const showPagination = total > pageSize; 
     return (
       <section className='px-4 py-6'>
         <div className='container-xl lg:container m-auto px-4 py-6'>
@@ -20,6 +25,10 @@ const PropertiesPage =  async() => {
                 }
             </div>
             )}
+            {showPagination && (
+                 <Pagination  page={parseInt(page)} pageSize={parseInt(pageSize)} totalItems={total}/>
+            )}
+       
         </div>
       
       </section>
